928771ef80cb55fdab81b0a79a8c862ed73f10b7
anisanhy <joyabrianrantepasang14@gmail.com>
2024-07-13 17:25:04 +0800

    add game timer

---
+++ Game/Game.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+onready var timer = $Timer
+
+var time = 0
+var second = 0
+var minute = 0
+var hour = 0
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	time += delta
+
+	second = fmod(time, 60)
+	minute = fmod(time / 60, 60)
+	hour = fmod(time / 3600, 24)
+
+	var hourText = "%02d" % hour
+	var minuteText = "%02d" % minute
+	var secondText = "%02d" % second
+
+	timer.text = hourText + ":" + minuteText + ":" + secondText
